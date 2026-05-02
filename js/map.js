e869f188d636e590b7ef67bed775f21b0ae2e7f8
Novyy <yanovyy1@gmail.com>
2026-05-02 19:05:13 +0300

    2

--- js/map.js
+++ js/map.js
@@ -1,6 +1,6 @@
-import * as THREE from 'https://cdn.skypack.dev/three@0.129.0/build/three.module.js';
-import { GLTFLoader } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/loaders/GLTFLoader.js';
-import { OrbitControls } from 'https://cdn.skypack.dev/three@0.129.0/examples/jsm/controls/OrbitControls.js';
+import * as THREE from 'three';
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js';
+import { OrbitControls } from 'three/addons/controls/OrbitControls.js';
 
 // 1. Настройка сцены
 const scene = new THREE.Scene();
